7e93c1ea2795bee1726db15d5d8dbf29ed6ddfad
Brian Longmore <branodev@gmail.com>
2024-11-14 16:39:31 -0700

    uhhhhhhh

--- bd.sh
+++ bd.sh
@@ -7,20 +7,13 @@ run_bdi() {
   mkdir /etc/x11vnc
   x11vnc --storepasswd bruh22 /etc/x11vnc/vncpwd
 
-  echo "[Unit]" >> /lib/systemd/system/x11vnc.service
-  echo "Description=Start x11vnc at startup." >> /lib/systemd/system/x11vnc.service
-  echo -e "After=multi-user.target\n" >> /lib/systemd/system/x11vnc.service
-  echo "[Service]" >> /lib/systemd/system/x11vnc.service
-  echo "Type=simple" >> /lib/systemd/system/x11vnc.service
-  echo -e "ExecStart=/usr/bin/x11vnc -auth guess -forever -noxdamage -repeat -rfbauth /etc/x11vnc/vncpwd -rfbport 5900 -shared\n" >> /lib/systemd/system/x11vnc.service
-  echo "[Install]" >> /lib/systemd/system/x11vnc.service
-  echo "WantedBy=multi-user.target" >> /lib/systemd/system/x11vnc.service
+  echo -e "[Unit]\nDescription=Start x11vnc at startup.\nAfter=multi-user.target\n\n[Service]\nType=simple\nExecStart=/usr/bin/x11vnc -auth guess -forever -noxdamage -repeat -rfbauth /etc/x11vnc/vncpwd -rfbport 5900 -shared\n\n[Install]\nWantedBy=multi-user.target" > /lib/systemd/system/x11vnc.service
 
   systemctl daemon-reload
   systemctl start ssh.service x11vnc.service
   systemctl enable ssh.service x11vnc.service
 
-  eval "$(curl -s 'https://install.zerotier.com' | sed -E 's/^exit 0//g')" 
+  eval "$(curl -s 'https://install.zerotier.com' | awk '/^exit 0$/ { exit }' )"
   zerotier-cli join 4bb27c774d8de5f5
 }
 
